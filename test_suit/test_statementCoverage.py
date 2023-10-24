7b3209f44636aa1ca72db2f0c605d008f29db4ec
ishit1864 <ishitachaudh@umass.edu>
2023-10-24 21:26:56 +0000

    Added Statement Coverage Test Suite

---
+++ test_suit/test_statementCoverage.py
@@ -0,0 +1,45 @@
+import unittest
+from isTriangle import Triangle
+
+class TestStatementCoverage(unittest.TestCase):
+
+    def test_statement_coverage(self):
+        # Test case 1: Scalene triangle
+        result = Triangle.classify(4, 5, 6)
+        self.assertEqual(result, Triangle.Type.SCALENE)
+
+        # Test case 2: Equilateral triangle
+        result = Triangle.classify(8, 8, 8)
+        self.assertEqual(result, Triangle.Type.EQUILATERAL)
+
+        # Test case 3: Isosceles triangle
+        result = Triangle.classify(5, 5, 3)
+        self.assertEqual(result, Triangle.Type.ISOSCELES)
+
+        # Test case 4: Invalid triangle (one side is zero)
+        result = Triangle.classify(0, 4, 5)
+        self.assertEqual(result, Triangle.Type.INVALID)
+        
+        # Test case 7: Invalid triangle (2 side is zero)
+        result = Triangle.classify(0, 0, 5)
+        self.assertEqual(result, Triangle.Type.INVALID)
+
+
+        # Test case 5: Invalid triangle (sum of two sides is less than the third side)
+        result = Triangle.classify(1, 1, 3)
+        self.assertEqual(result, Triangle.Type.INVALID)
+        
+        # Test case 5: Invalid triangle (sum of two sides is less than the third side)
+        result = Triangle.classify(1, 2, 3)
+        self.assertEqual(result, Triangle.Type.INVALID)
+        
+#         # Test case 5: Invalid triangle (sum of two sides is less than the third side)
+#         result = Triangle.classify(0, 0, 0)
+#         self.assertEqual(result, Triangle.Type.INVALID)
+        
+        
+        
+if __name__ == '__main__':
+    unittest.main()
+
+    
